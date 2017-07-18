a59a2602e99a080a02d2b11353bc2d6cc7fb0394
LeonBirk <leonbirk@outlook.com>
2017-07-18 18:16:23 +0200

    Initial commit

---
+++ messages/index.js
@@ -0,0 +1,135 @@
+"use strict";
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+var path = require('path');
+var request = require('request');
+var headers = {
+    'Accept': 'application/json'
+};
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+var bot = new builder.UniversalBot(connector);
+bot.localePath(path.join(__dirname, './locale'));
+
+bot.dialog('/', function (session) {
+    if(session.message.text.includes("INC")){
+    session.send("Getting Incident data...");
+    var incidentID = session.message.text;
+    //session.send('You requested information on the Incident with the ID ' + session.message.text);
+    var urlString = 'https://dev27563.service-now.com/api/now/table/incident?sysparm_query=number=' + incidentID; 
+var options = {
+    url: urlString,
+    headers: headers,
+    auth: {
+        'user': 'admin',
+        'pass': 'EF3tGqL5T!'
+    }
+};
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var respJSON = JSON.parse(body);
+        //session.send(body)
+        var state = "empty";
+        state = respJSON.result[0].state;
+        switch(state){
+            case "1": 
+                state = "New";
+                break;
+            case "2":
+                state = "In Progress";
+                break;
+            case "3":
+                state = "On Hold";
+                break;
+            case "4":
+                state = "Resolved";
+                break;
+            case "5":
+                state = "Closed";
+                break;
+            case "6":
+                state = "Canceled";
+                break;
+            default:
+                state = "undefined";
+        }
+        session.send('Requested ID: ' + respJSON.result[0].number + '\n Status: ' + state + '\n Urgency: '+ respJSON.result[0].urgency + '\n Short Description: ' + respJSON.result[0].short_description);
+    }
+}
+
+request(options, callback);
+    } else if (session.message.text.includes("my incidents")){
+        session.send("Getting your personal incidents...");
+        var urlString = 'https://dev27563.service-now.com/api/now/table/incident?sysparm_query=caller_id=681ccaf9c0a8016400b98a06818d57c7'; 
+        var options = {
+            url: urlString,
+            headers: headers,
+            auth: {
+                'user': 'admin',
+                'pass': 'EF3tGqL5T!'
+            }
+            };
+            
+    function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var respJSON = JSON.parse(body);
+        //session.send(body);
+        var incidentCount = respJSON.result.length;
+        session.send("You currently have " + incidentCount + " incidents.")
+        for (var i = 0; i<respJSON.result.length; i++){
+            session.send("Incident ID number " + (i+1) + " is: " + respJSON.result[i].number + ", short description is: "+ respJSON.result[i].short_description);
+        }
+        session.send("If you want more information on one of those incidents, ask me about its ID.")
+    }
+    }
+    request(options, callback);
+    }
+    else if (session.message.text.includes("order sales laptop")){
+        session.send("Adding to cart: Sales Laptop...");
+        var body = {'sysparm_quantity': '1'};
+        var urlString = 'https://dev27563.service-now.com/api/sn_sc/servicecatalog/items/e212a942c0a80165008313c59764eea1/add_to_cart'; 
+        var options = {
+            url: urlString,
+            method: 'POST',
+            json: true,
+            data: body,
+            headers: headers,
+            auth: {
+                'user': 'admin',
+                'pass': 'EF3tGqL5T!'
+            }
+            };
+            
+    function callback(error, response, body) {
+        session.send(body);
+        if (!error && response.statusCode == 200) {
+            var respJSON = JSON.parse(body);
+            session.send(body);
+        }
+    }
+    request(options, callback);
+    }
+    else {
+        session.send("I'm afraid I didn't understand. You can either list your incidents through the keyphrase: ''my incidents'' or search for a specific incident through ID.");
+    }
+
+});
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = { default: connector.listen() }
+}
