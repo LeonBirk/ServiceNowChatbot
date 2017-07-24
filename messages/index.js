2c5445f4729a184e554c65bd5221e4a1fe227dc2
LeonBirk <leonbirk@outlook.com>
2017-07-24 16:07:18 -0400

    ORDER HARDWARE: 0.411 json structure fix

--- messages/index.js
+++ messages/index.js
@@ -356,7 +356,7 @@ bot.dialog('orderHardware', [
 
     function (session, result){
         session.dialogData.hardwareCategory = result.response.entity;
-        var choices = hardware[session.dialogData.hardwareCategory];
+        var choices = hardware[session.dialogData.hardwareCategory].value;
         builder.Prompts.choice(session, 'Nice! What type of device do you want to order?', choices)
     },
 
