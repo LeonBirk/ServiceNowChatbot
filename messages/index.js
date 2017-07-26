e5cfb2565611bd72d5424d7d54560f3b015aff5d
LeonBirk <leonbirk@outlook.com>
2017-07-26 11:29:31 -0400

    ORDER HARDWARE: 0.9 Working with answer body

--- messages/index.js
+++ messages/index.js
@@ -386,15 +386,15 @@ bot.dialog('orderHardware', [
             }
         };
         function callback(error, response, body) {
-            session.send("Callback function is called.");
+            //session.send("Callback function is called.");
             if (!error && response.statusCode == 200) {
-                session.send(session.dialogData.hardwareDevice.toString() + " has been put into your personal cart.");
+                session.send(session.dialogData.hardwareSubcategory.toString() + ": " +session.dialogData.hardwareDevice.toString() + " has been put into your personal cart.");
                 var answer = body;
                 session.send("You currently have " + answer.result.items.length + " items in your cart:");
                 for (var i = 0; i < answer.result.items.length; i++) {
                     session.send(answer.result.items[i].item_name + " for " + answer.result.items[i].localized_price)
                 }
-                builder.Prompts.choice(session, "The subtotal is" + answer.result.subtotal + "Are you ready to proceed to checkout?", isThatCorrect);
+                builder.Prompts.choice(session, "The subtotal is " + answer.result.subtotal + ". Are you ready to proceed to checkout?", isThatCorrect);
 
             }
         }
